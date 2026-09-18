5c854c6f4ed76b3930893c58bcfc4ceda5c202b9
Chase Andeen <chase.andeen@proton.me>
2026-09-18 00:30:20 +0000

    Add Player class and integrate into main game loop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
-from   constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from   logger    import log_state
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from logger import log_state
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,8 +11,11 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock  = pygame.time.Clock()
-    dt     = 0.0
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
 
     while True:
         log_state()
@@ -20,8 +25,13 @@ def main():
                 return
 
         screen.fill("black")
+
+        player = Player(x, y)
+        player.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
