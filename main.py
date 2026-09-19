abd97c019c4ab4755cdd097334e2e4b8aff370c3
Chase Andeen <chase.andeen@proton.me>
2026-09-19 03:44:02 +0000

    Refactor main game loop and player initialization for improved structure

--- main.py
+++ main.py
@@ -5,19 +5,26 @@ from logger    import log_state
 
 
 def main():
+    # --- Info ---
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # --- Initialization ---
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable  = pygame.sprite.Group()
+    drawable   = pygame.sprite.Group()
 
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+    # --- Player Initialization ---
+    Player.containers = (updatable, drawable)
+    x                 = SCREEN_WIDTH / 2
+    y                 = SCREEN_HEIGHT / 2
+    player            = Player(x, y)
 
+    # --- Game Loop ---
     while True:
         log_state()
 
@@ -26,8 +33,9 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
