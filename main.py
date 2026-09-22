4cd4fc15b7cda6cdfac374683445ee6b05ad03dc
Chase Andeen <chase.andeen@proton.me>
2026-09-22 17:01:36 +0000

    Implement asteroid splitting and collision handling with shots

--- main.py
+++ main.py
@@ -55,6 +55,12 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
+
         for object in drawable:
             object.draw(screen)
         pygame.display.flip()
--- player.py
+++ player.py
@@ -1,13 +1,14 @@
 import pygame
 from circleshape import CircleShape
 from shot        import Shot
-from constants   import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants   import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 
 
 class Player(CircleShape):
     def __init__(self, x: float, y: float):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation = 0
+        self.rotation      = 0
+        self.shot_cooldown = 0
 
     def triangle(self) -> list[pygame.Vector2]:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -31,6 +32,8 @@ class Player(CircleShape):
         self.position            += rotated_with_speed_vector
 
     def update(self, dt: float) -> None:
+        self.shot_cooldown -= dt
+        
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -45,7 +48,7 @@ class Player(CircleShape):
             self.shoot()
 
     def shoot(self) -> None:
-        current_shot = Shot(self.position.x, self.position.y)
-        current_shot.velocity  = pygame.Vector2(0,1)
-        current_shot.velocity  = pygame.math.Vector2.rotate(current_shot.velocity, self.rotation)
-        current_shot.velocity *= PLAYER_SHOOT_SPEED
+        if self.shot_cooldown <= 0:
+            current_shot = Shot(self.position.x, self.position.y)
+            current_shot.velocity  = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.shot_cooldown     = PLAYER_SHOOT_COOLDOWN_SECONDS
